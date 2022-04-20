7abd4d2a5021ec10564e3d202294f944f392412a
SebThiertant <sebastien.thiertant@gmail.com>
2022-04-20 23:16:15 +0200

    Add button with useEffect

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,19 +1,32 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import { useState } from 'react'
 
 const HomePage = () => {
+  const [name, setName] = useState('Defaut')
+
+  // Random a name
+  const onClick = () => {
+    if (name === 'Defaut') {
+      setName('Blabla')
+    } else {
+      setName('Defaut')
+    }
+  }
+
   return (
     <>
       <MetaTags title="Home" description="Home page" />
 
-      <h1>Test Home page</h1>
+      <h1>Test Home with {name} </h1>
       <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
+        Find me at <code>./web/src/pages/HomePage/HomePage.js</code>
       </p>
       <p>
         My default route is named <code>home</code>, link to me with `
         <Link to={routes.home()}>Home</Link>`
       </p>
+      <button onClick={onClick}>Click on me</button>
     </>
   )
 }
